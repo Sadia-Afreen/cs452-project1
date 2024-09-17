502c135c3c50879db4462d809922526b45f7d60e
Sadia Afreen <sadiaafreen@u.boisestate.edu>
2024-09-17 01:20:48 -0600

    Implemented task3,4,5

--- app/main.c
+++ app/main.c
@@ -2,35 +2,46 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
+#include "/home/sadiaafreen/Documents/BSU/Fall_24/Operating Systems/cs452-project1/src/lab.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int main(int argc, char **argv)
 {
-  printf("hello world\n");
   int c;
 
-  while ((c = getopt (argc, argv, "abc:")) != -1)
+  while ((c = getopt(argc, argv, "v:")) != -1)
   {
     switch (c)
     {
-    case 'a':
-      printf("get a");
-      break;
-    case 'b':
-      printf("get b");
-      break;
-    case 'c':
-      printf("get c");
+    case 'v':
+      printf("Shell version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+      return 0;
       break;
     case '?':
       if (isprint(optopt))
         fprintf(stderr, "Unknown", optopt);
       else
         fprintf(stderr, "Unknown", optopt);
-    return 1;
+      return 1;
     default:
-      abort ();
+      abort();
     }
   }
-  
+
+  struct shell my_shell;
+  my_shell.prompt = get_prompt("MY_PROMPT");
+  // printf("Prompt: %s\n", my_shell.prompt);
+
+  char *line;
+  using_history();
+  while ((line = readline(my_shell.prompt)))
+  {
+    printf("%s\n", line);
+    add_history(line);
+    free(line);
+  }
+  free(my_shell.prompt);
+
   return 0;
 }
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,40 @@
-/**Update this file with the starter code**/
+#include "/home/sadiaafreen/Documents/BSU/Fall_24/Operating Systems/cs452-project1/src/lab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Set the shell prompt. This function will attempt to load a prompt
+ * from the requested environment variable, if the environment variable is
+ * not set a default prompt of "shell>" is returned.  This function calls
+ * malloc internally and the caller must free the resulting string.
+ *
+ * @param env The environment variable
+ * @return const char* The prompt
+ */
+char *get_prompt(const char *env)
+{
+  char *prompt = NULL;
+  const char *default_prompt = "shell> ";
+
+  const char *env_prompt = getenv(env);
+
+  if (env_prompt != NULL)
+  {
+    prompt = malloc(strlen(env_prompt) + 1);
+    if (prompt != NULL)
+    {
+      strcpy(prompt, env_prompt);
+    }
+  }
+  else
+  {
+    prompt = malloc(strlen(default_prompt) + 1);
+    if (prompt != NULL)
+    {
+      strcpy(prompt, default_prompt);
+    }
+  }
+
+  return prompt;
+}
